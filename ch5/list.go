599a369bd8583002e563eb3373d976a74d64197a
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-04-27 16:08:53 +0200

    filter added

--- ch5/list.go
+++ ch5/list.go
@@ -3,16 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var slice = make([]int, 3, 3)
-	fmt.Println(slice)
+	var slice = []int{1,2,3,4,5}
+
+	fmt.Printf("orig:   %v\n", slice)
 
 	var addOne = func(x int) int {
 		return x + 1
 	}
-	fmt.Println(fmap(slice, addOne))
+	fmt.Printf("fmap:   %v\n", fmap(slice, addOne))
 
-	f := dups()
-	fmt.Println(bind(slice, f))
+	var odd = func(x int) bool {
+		return x % 2 == 1
+	}
+	fmt.Printf("filter: %v\n", filter(slice, odd))
+
+	var even = func(x int) bool {
+		return x % 2 == 0
+	}
+	fmt.Printf("filter: %v\n", filter(slice, even))
+
+	// f := dups()
+	// fmt.Println(bind(slice, f))
 }
 
 func dups() func(i int) []int {
@@ -35,6 +46,18 @@ func fmap(ls []int, f func(i int) int) []int {
 	return res
 }
 
+// TODO implement
+func filter(ls []int, p func(int) bool) []int {
+	res := make([]int, len(ls))
+	pos := 0
+	for _, val := range ls {
+		if (p(val)) {
+			res[pos] = val
+			pos ++
+		}
+	}
+	return res[:pos]
+}
 
 func bind(ls []int, f func(i int) []int) []int {
 	return ls
