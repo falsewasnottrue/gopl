1df3aa7d3c94bc75ef27ac0d1f24c4a90b0b1bd0
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-04-29 11:14:57 +0200

    Head(), Tail() methods

--- ch6/intlist.go
+++ ch6/intlist.go
@@ -10,12 +10,19 @@ type IntList struct {
 	Next *IntList
 }
 
-// TODO build
 func build(vals ...int) *IntList {
-	return nil
+	var res *IntList
+	for i := len(vals)-1; i>=0; i-- {
+		l := new (IntList)
+		l.Value = vals[i]
+		l.Next = res
+
+		res = l
+	}
+
+	return res
 }
 
-// TODO toString
 func (list *IntList) String() string {
 	if (list == nil) {
 		return "."
@@ -28,20 +35,35 @@ func (list *IntList) String() string {
 	return strconv.Itoa(list.Value) + "," + list.Next.String()
 }
 
-// TODO head
-// TODO tail
-// TODO last
+func (list *IntList) Head() int {
+	if (list == nil) {
+		panic("no head on empty list")
+	}
+	return list.Value
+}
+
+func (list *IntList) Tail() *IntList {
+	if (list == nil) {
+		return nil
+	}
+	return list.Next
+}
+
 // TODO append
+func (list *IntList) Append(other *IntList) *IntList {
+
+}
+
 // TODO fold
 // TODO fmap
 // TODO filter
 // TODO bind
 
 func main() {
-	i := 42
-	fmt.Println(string(i))
-
 	l := build(1,2,3)
 
 	fmt.Printf("%v\n", l)
+	fmt.Println(l.Head())
+	fmt.Printf("%v\n", l.Tail())
+	fmt.Println(l.Tail().Head())
 }
