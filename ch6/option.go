e8c3a752237a1f44095ae242f32e9dd37ec51d69
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-05-02 14:26:12 +0200

    filter added

--- ch6/option.go
+++ ch6/option.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 type IntOption interface {
 	Get() int
 	GetOrElse(i int) int
 
+	Filter(p func(val int) bool) IntOption
+
 	// TODO FMap
-	// TODO Filter
 	// TODO Bind
 }
 
+// --- Some
 type Some struct {
 	value int
+}
+
+func (ctx Some) String() string {
+	return "Some(" + strconv.Itoa(ctx.value) + ")"
 }
 
 func (ctx Some) Get() int {
@@ -25,8 +32,20 @@ func (ctx Some) GetOrElse(i int) int {
 	return ctx.value
 }
 
-// TODO implementation None
+func (ctx Some) Filter(p func(val int) bool) IntOption {
+	if p(ctx.value) {
+		return ctx
+	} else {
+		return None{}
+	}
+}
+
+// --- None
 type None struct {}
+
+func (ctx None) String() string {
+	return "None"
+}
 
 func (ctx None) Get() int {
 	panic("Cannot access value of None")
@@ -36,11 +55,35 @@ func (ctx None) GetOrElse(i int) int {
 	return i
 }
 
+func (ctx None) Filter(f func(i int) bool) IntOption {
+	return ctx
+}
+
+// ---
 func main() {
+	var isEven = func(val int) bool {
+		return val % 2 == 0
+	}
+	var isOdd = func(val int) bool {
+		return val % 2 == 1
+	}
+
+	// ---
 	var someone = Some{value: 1}
 	
 	fmt.Printf("%t %v\n", someone, someone)
-	fmt.Println(someone.Get())
-	fmt.Println(someone.GetOrElse(42))
+	fmt.Printf("Some(1).get: %v\n", someone.Get())
+	fmt.Println("Some(1).getOrElse(42): %v\n", someone.GetOrElse(42))
+
+	fmt.Printf("Some(1).filter(isEven): %v\n", someone.Filter(isEven))
+	fmt.Printf("Some(1).filter(isOdd): %v\n", someone.Filter(isOdd))
+
+	// ---
+
+	var noone = None{}
+
+	fmt.Printf("%t %v\n", noone, noone)
+	// fmt.Println(noone.Get()) -> panic
+	fmt.Printf("None.getOrElse(42): %v\n", noone.GetOrElse(42))
 
 }
